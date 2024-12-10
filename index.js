51b147b9c451f8f5499dbcb351a8b7171eecfd22
Srdjan S <stankovic.srdjo@gmail.com>
2024-12-10 17:27:38 +0100

    Simplify request formating code

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 import zlib from 'node:zlib';
 import { promisify } from 'node:util';
 
-function getRequestData(request, response) {
+function formatRequestData(request, response, env) {
+  const url = new URL(request.url);
   const timestamp = Date.now();
   const originResponse = response || {};
-  const url = new URL(request.url);
   const apiKey = request.headers.get('api-key') || url.searchParams.get('api-key') || (request.headers.get('Authorization')?.startsWith('Bearer ') ? request.headers.get('Authorization').substring(7) : 'unknown');
-
-  return {
-    'timestamp': timestamp,
-    'url': request.url,
-    'method': request.method,
-    'status': originResponse.status,
-    'cfCache': (originResponse) ? (response.headers.get('CF-Cache-Status') || 'miss') : 'miss',
-    'apiKey': apiKey,
-  };
-}
-
-function formatRequestData(data, env) {
-  const url = new URL(data.url);
+  const cfCache = (originResponse) ? (response.headers.get('CF-Cache-Status') || 'miss') : 'miss'
   const formattedUrl = url.toString().replaceAll('=', '\\=');
+
   // We're setting field value to 1 to count the number of requests
-  return `${env.INFLUX_METRIC},status_code=${data.status},url=${formattedUrl},hostname=${url.hostname},pathname="${url.pathname}",method=${data.method},cf_cache=${data.cfCache},api_key=${data.apiKey} value=1 ${data.timestamp}`
+  return `${env.INFLUX_METRIC},status_code=${originResponse.status},url=${formattedUrl},hostname=${url.hostname},pathname="${url.pathname}",method=${request.method},cf_cache=${cfCache},api_key=${apiKey} value=1 ${timestamp}`
 }
 
 async function reportMetric(request, response, env) {
   const compress = promisify(zlib.gzip);
-  const reqData = getRequestData(request, response);
-  const line = formatRequestData(reqData, env);
+  const requestData = formatRequestData(request, response, env);
 
   // Define API endpoint and headers
   const url = `${env.INFLUX_URL}/api/v2/write?&bucket=${env.INFLUX_DATABASE}&precision=ms`;
 
   // Compress the string using gzip
-  const compressedData = await compress(line);
+  const compressedData = await compress(requestData);
 
   // Make the POST request
   return fetch(url, {
