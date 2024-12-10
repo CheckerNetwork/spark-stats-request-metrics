4c547d7fd7c9b439de0769ef15dc38a328efee18
Srdjan S <stankovic.srdjo@gmail.com>
2024-12-10 17:29:19 +0100

    Remove unused function arguments

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function reportMetric(request, response, env) {
 }
 
 
-async function handleRequest(request, env, ctx) {
+async function handleRequest(request, env) {
   const url = new URL(request.url);
   const newRequest = new Request(`${env.REQUEST_URL}${url.pathname}${url.search}`, request);
 
@@ -51,12 +51,12 @@ export default {
     if (request.method === 'GET') {
       response = await cache.match(request);
       if (!response) {
-        response = await handleRequest(request, env, ctx);
+        response = await handleRequest(request, env);
         ctx.waitUntil(cache.put(request, response.clone()));
       }
 
     } else {
-      response = await handleRequest(request, env, ctx);
+      response = await handleRequest(request, env); 
     }
 
     // report metrics for original request
